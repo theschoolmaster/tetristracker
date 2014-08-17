ea199e293243048d86efd2809541235d1f730391
Matthew Schoolmaster <schoolmaster@gmail.com>
2014-08-17 16:23:25 -0600

    add in pagination

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,7 +5,7 @@ class GamesController < ApplicationController
   # GET /games
   # GET /games.json
   def index
-    @games = Game.all
+    @games = Game.paginate(page: params[:page], per_page: 20)
   end
 
   # GET /games/1
