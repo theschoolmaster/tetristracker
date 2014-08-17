2fa3ae8be1e44ee4f39fa2909860d237aa7999b5
Matthew Schoolmaster <schoolmaster@gmail.com>
2014-08-17 17:34:42 -0600

    calculating finish level based on lines

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,15 @@ module ApplicationHelper
     super *[collection_or_options, options].compact
   end
 
+  def days_ago(date)
+    days = ((Time.now - date) / 24 / 60 / 60).round
+
+    case days
+      when 0    then time_ago_in_words(date)
+      when 1    then 'yesterday'
+      else           "#{days}d"
+    end
+  end
+
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,14 @@ class Game < ActiveRecord::Base
   belongs_to :user
 
   default_scope {  order( created_at: :desc) }
+
+  def calculated_finish_level
+    unless lines.blank?
+      if TETRIS_LEVEL_ADVANCE[start_level] > lines
+        start_level
+      else
+        ( (lines - TETRIS_LEVEL_ADVANCE[start_level])/10 + start_level + 1).floor
+      end
+    end
+  end
 end
